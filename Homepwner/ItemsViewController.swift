f32cabc43c97ff49abc84ba08189fa4ded24a527
saroj <bipin@dronger.local>
2016-06-01 14:53:32 +0545

    editing and add in tableview

--- Homepwner/ItemsViewController.swift
+++ Homepwner/ItemsViewController.swift
@@ -3,13 +3,49 @@ import UIKit
 class ItemsViewController : UITableViewController {
     var itemStore : ItemStore!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let statusBarHeight = UIApplication.sharedApplication().statusBarFrame.height
+        
+        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
+        tableView.contentInset = insets
+        tableView.scrollIndicatorInsets = insets
+    }
+    
+    @IBAction func addNewItem(sender: AnyObject) {
+        
+        let newItem = itemStore.createItem()
+        
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+            
+        }
+        
+        
+    }
+    
+    @IBAction func toggleEditingMode(sender: AnyObject) {
+        if editing {
+            sender.setTitle("Edit", forState:  .Normal)
+            setEditing(false, animated: true)
+        }
+        else {
+            sender.setTitle("Done", forState: .Normal)
+            setEditing(true, animated: true)
+        }
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+//        let cell = UITableViewCell(style: .Value1, reuseIdentifier: "UITableViewCell")
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
         
         let item = itemStore.allItems[indexPath.row]
         
@@ -17,5 +53,11 @@ class ItemsViewController : UITableViewController {
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
         
         return cell
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+//            let item = itemStore.allItems[indexPath.]
+        }
     }
 }
